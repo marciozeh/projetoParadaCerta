1f9991d53e9fb40524040091a1f5a1e0b582288d
marciozeh <zehgomes@hotmail.com>
2017-10-03 19:39:18 -0300

    rotas devidamente funcionando, agora aparecem no mapa todas as paradas do mapa selecionado.

--- app/src/main/java/marcio/com/br/paradacertaprojeto/MapsActivity.java
+++ app/src/main/java/marcio/com/br/paradacertaprojeto/MapsActivity.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -67,6 +68,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 LatLng sydney = new LatLng(latitude, longitude);
                 mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
                 //Log.i("LogX", "latitude: " + cursor.getString(indiceColunaLatitude) + " longitude: " + cursor.getString(indiceColunaLongitude));
                 cursor.moveToNext();
