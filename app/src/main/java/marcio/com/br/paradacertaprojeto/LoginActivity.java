3ec8ac5c028e622765ee6639a5ea39710774e4d8
Erick <eodsilva@restinga.ifrs.edu.br>
2017-10-07 15:58:07 -0300

    FaceBook LoginActivity

--- app/src/main/java/marcio/com/br/paradacertaprojeto/LoginActivity.java
+++ app/src/main/java/marcio/com/br/paradacertaprojeto/LoginActivity.java
@@ -4,5 +4,9 @@ package marcio.com.br.paradacertaprojeto;
  * Created by Erick on 07/10/2017.
  */
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class LoginActivity {
+
 }
