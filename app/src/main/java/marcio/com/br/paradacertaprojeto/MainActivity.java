470f08d017f3984b3b68a9b0d0246a956e508988
Erick <eodsilva@restinga.ifrs.edu.br>
2017-10-07 20:22:23 -0300

    FaceBook Entrar/Sair Perfil

--- app/src/main/java/marcio/com/br/paradacertaprojeto/MainActivity.java
+++ app/src/main/java/marcio/com/br/paradacertaprojeto/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,9 +21,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
     static ArrayAdapter arrayAdapter;
     public static String linhaid;
 
+    private LoginButton loginButton;
+    private FirebaseAuth fireBaseAuth;
+    private CallbackManager callbackManager;
 
     private ListView listaLinhas;
     private ArrayAdapter<String> itensAdaptador;
@@ -59,12 +76,73 @@ public class MainActivity extends AppCompatActivity {
         listaLinhas = (ListView) findViewById(R.id.listviewid);
         carregaLinhas();
 
-        if (AccessToken.getCurrentAccessToken() == null) {
-            goLoginScreen();
-        }
+        inicializarComponente();
+        inicializarFirebaseCallback();
+        clickButton();
+        
+//        if (AccessToken.getCurrentAccessToken() == null) {
+//            goLoginScreen();
+//        }
 
     }
 
+    private void clickButton() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                firebaseLogin(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                alert("Operação Cancelada");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                alert("Erro no login com o Facebook");
+            }
+        });
+
+    }
+
+    private void firebaseLogin(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        fireBaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent i = new Intent(MainActivity.this,Perfil.class);
+                    startActivity(i);
+                }else{
+                    alert("Erro de Autenticação com o Firebase");
+
+                }
+            }
+        });
+    }
+
+    private void alert(String s) {
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+    }
+
+    private void inicializarFirebaseCallback() {
+        fireBaseAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
+
+    }
+
+
+    private void inicializarComponente() {
+        loginButton = (LoginButton) findViewById(R.id.btnLogin);
+        loginButton.setReadPermissions("email","public_profile");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
 
     //Carrega a lista de linhas disponíveis, nela será possível escolher a linha necessária para carregar as paradas a seguir.
@@ -440,14 +518,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void goLoginScreen() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
-    public void logout(View view) {
-        LoginManager.getInstance().logOut();
-        goLoginScreen();
-    }
+//    private void goLoginScreen() {
+//        Intent intent = new Intent(this, LoginActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//        startActivity(intent);
+//    }
+//
+//    public void logout(View view) {
+//        LoginManager.getInstance().logOut();
+//        goLoginScreen();
+//    }
 }
