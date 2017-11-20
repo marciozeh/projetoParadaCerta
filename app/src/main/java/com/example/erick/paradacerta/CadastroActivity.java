47a836a1818e10cd45da1431395f384b446d337e
marciozeh <zehgomes@hotmail.com>
2017-11-20 21:10:24 -0200

    projeto alterado na aula com erick

--- app/src/main/java/com/example/erick/paradacerta/CadastroActivity.java
+++ app/src/main/java/com/example/erick/paradacerta/CadastroActivity.java
@@ -4,16 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -41,9 +40,9 @@ public class CadastroActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        inicializarComponetes();
-        inicializarFirebase();
-        clickButton();
+//        inicializarComponetes();
+//        inicializarFirebase();
+//        clickButton();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -101,58 +100,58 @@ public class CadastroActivity extends AppCompatActivity
         Glide.with(CadastroActivity.this).load(mFirebaseUser.getPhotoUrl()).into(ivFoto);
     }
 
-    private void inicializarComponetes() {
-        ivFoto = (ImageView) findViewById(R.id.ivFoto);
-        tvEmail = (TextView)  findViewById(R.id.tvEmail);
-        tvId = (TextView)  findViewById(R.id.tvId);
-        tvCpf = (TextView)  findViewById(R.id.tvCpf);
-        tvDtNascimento = (TextView)  findViewById(R.id.tvDtNascimento);
-        btnLogOut = (Button) findViewById(R.id.btnLogOut);
-    }
+//    private void inicializarComponetes() {
+//        ivFoto = (ImageView) findViewById(R.id.ivFoto);
+//        tvEmail = (TextView)  findViewById(R.id.tvEmail);
+//        tvId = (TextView)  findViewById(R.id.tvId);
+//        tvCpf = (TextView)  findViewById(R.id.tvCpf);
+//        tvDtNascimento = (TextView)  findViewById(R.id.tvDtNascimento);
+//        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+//    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.maps, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.maps, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -165,7 +164,7 @@ public class CadastroActivity extends AppCompatActivity
                 startActivity(h);
                 break;
             case R.id.nav_cadastro:
-                Intent i = new Intent(CadastroActivity.this,MapsActivity.class);
+                Intent i = new Intent(CadastroActivity.this,CadastroActivity.class);
                 startActivity(i);
                 break;
         }
--- app/src/main/java/com/example/erick/paradacerta/MapsActivity.java
+++ app/src/main/java/com/example/erick/paradacerta/MapsActivity.java
@@ -8,20 +8,17 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -103,7 +100,6 @@ public class MapsActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
 
 
         if (savedInstanceState != null) {
@@ -129,7 +125,7 @@ public class MapsActivity extends AppCompatActivity
         // Getting reference to EditText
         etPlace = (EditText) findViewById(R.id.et_place);
 
-        // Setting click event listener for the find button
+//         Setting click event listener for the find button
         mBtnFind.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -170,27 +166,28 @@ public class MapsActivity extends AppCompatActivity
         //carregaLinhas();
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//
+//        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.setDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+//
+  }
 
     @Override
     public void onBackPressed() {
@@ -235,7 +232,7 @@ public class MapsActivity extends AppCompatActivity
                 startActivity(h);
                 break;
             case R.id.nav_cadastro:
-                Intent i = new Intent(MapsActivity.this,MapsActivity.class);
+                Intent i = new Intent(MapsActivity.this,CadastroActivity.class);
                 startActivity(i);
                 break;
         }
@@ -270,6 +267,8 @@ public class MapsActivity extends AppCompatActivity
 
         // Get the current location of the device and set the position of the map.
         getDeviceLocation();
+
+        //mostraLinhas();
 
     }
 
